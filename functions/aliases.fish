2a2361074748594d61e46feee7d88379cc80c832
anthony <anthonytran.dev@gmail.com>
2020-10-19 02:54:27 +0100

    removed unnecessary shebang

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Navigation
 alias ... "cd ../../"
 alias .... "cd ../../../"
@@ -7,4 +5,8 @@ alias .... "cd ../../../"
 # Shortcuts
 alias dl="cd ~/Downloads"
 alias dt="cd ~/Desktop"
+
+# Git
+alias ga="git add ."
+alias gm="git commit -m"
 
